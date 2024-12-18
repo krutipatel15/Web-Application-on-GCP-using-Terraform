79563a6760f8df284b522ff87e96fd7dd6c03dd9
kruti <krutidpatel153@gmail.com>
2024-12-18 00:18:57 -0500

    updating variable file

--- variables.tf
+++ variables.tf
@@ -1,6 +1,22 @@
-variable "project-id" {}
-variable "region" {}
-variable "vpc_network_name" {}
+variable "project-id" {
+    type = string
+    description = "It's a GCP Project ID"
+    sensitive = true
+}
+variable "region" {
+    type = string
+    description = "Region in which the service will be deployed"
+}
+variable "vpc_network_name" {
+    type = string
+    description = "VPC network name"
+}
 variable "vpc_network_description" {}
-variable "public_subnet_name" {}
-variable "public_subnet_ip_range" {}
+variable "public_subnet_name" {
+    type = string
+    description = "public-subnet name"
+}
+variable "public_subnet_ip_range" {
+    type = number
+    description = "ip range for public-subnet"
+}
